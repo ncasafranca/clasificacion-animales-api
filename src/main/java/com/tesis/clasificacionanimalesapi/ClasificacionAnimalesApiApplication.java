7f765d33fbfe008698aa7f3547edbf9dcb3bd5a5
Niel Casafranca <u202014690@upc.edu.pe>
2023-06-06 11:54:13 -0500

    feat:inicial:

---
+++ src/main/java/com/tesis/clasificacionanimalesapi/ClasificacionAnimalesApiApplication.java
@@ -0,0 +1,13 @@
+package com.tesis.clasificacionanimalesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClasificacionAnimalesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClasificacionAnimalesApiApplication.class, args);
+	}
+
+}
