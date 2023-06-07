7130a8dc904b533da3d02a45a16dd3aaff0e52ca
Niel Casafranca <u202014690@upc.edu.pe>
2023-06-06 21:08:54 -0500

    feat: refactor code

--- src/main/java/com/tesis/clasificacionanimalesapi/model/User.java
+++ src/main/java/com/tesis/clasificacionanimalesapi/model/User.java
@@ -18,9 +18,7 @@ public class User {
     private String rpassword;
     private String phoneNumber;
 
-    public User() {
-
-    }
+    public User() { }
 
     public User(String firstName,
                 String lastName, String role, String documentNumber, String email, String password, String rpassword, String phoneNumber) {
@@ -36,29 +34,16 @@ public class User {
 
     }
 
-    public String getId() {
-        return id;
-    }
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
 
-    public void setId(String id) {
-        this.id = id;
-    }
+    public String getFirstName() { return firstName; }
 
-    public String getFirstName() {
-        return firstName;
-    }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
+    public String getLastName() { return lastName; }
 
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
+    public void setLastName(String lastName) { this.lastName = lastName; }
 
     public String getRole() { return role; }
 
--- src/main/java/com/tesis/clasificacionanimalesapi/repository/UserRepository.java
+++ src/main/java/com/tesis/clasificacionanimalesapi/repository/UserRepository.java
@@ -3,6 +3,4 @@ package com.tesis.clasificacionanimalesapi.repository;
 import com.azure.spring.data.cosmos.repository.CosmosRepository;
 import com.tesis.clasificacionanimalesapi.model.User;
 
-public interface UserRepository extends CosmosRepository<User, String> {
-
-}
+public interface UserRepository extends CosmosRepository<User, String> { }
